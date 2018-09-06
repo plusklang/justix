954202005ed046b2df5948bee35b03818d8c73ca
Thomas Plucinski <>
2018-09-06 14:49:54 +0200

    Validation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,9 @@
 
 "use strict";
 
+
+//**************** HTML elements ********************//
+
 let headerElements= {};
 headerElements["$header"] = document.getElementById('header');
 headerElements["$hamburger"] = document.getElementById('hamburger');
@@ -12,20 +15,29 @@ loginElements["$btnReveal"] = loginElements.$form.getElementsByClassName('form__
 loginElements["$requirementsInfo"] = loginElements.$form.getElementsByClassName('form__requirements');
 loginElements["$revealButton"] = loginElements.$form.getElementsByClassName('form__btn-reveal');
 
-//Event listeners
 
-headerElements.$hamburger.addEventListener('click', function(e){
-    hamburgerHandler(e);
-});
+//************* Event listeners *************//
+
+headerElements.$hamburger.addEventListener('click', hamburgerHandler);
+
 
 for(let el = 0; el < loginElements.$revealButton.length; el++){
 
-    loginElements.$revealButton[el].addEventListener('click', function(e){
-        revealBtnHandler(e);
-    });
+    loginElements.$revealButton[el].addEventListener('click', revealBtnHandler);
 }
 
-//Event Handler functions
+for(let el = 0; el < loginElements.$inputs.length; el++){
+    loginElements.$inputs[el].addEventListener('blur', inputBlurHandler);
+}
+
+loginElements.$form.addEventListener('submit', formSubmitHandler);
+
+//*********** Event Handler functions *************//
+
+function inputBlurHandler(e){
+    let $input = e.target;
+    setOccupiedInput($input.value, $input);
+}
 
 /**
  * Handles the hamburger click event
@@ -45,7 +57,6 @@ function hamburgerHandler(e){
 function revealBtnHandler(e) {
     let $revealBtn = e.target;
     let $input = $revealBtn.parentNode.getElementsByClassName("form__input")[0];
-    alert('reveal button');
     if ($revealBtn.classList.toggle("form__btn-reveal--revealed")) {
         $input.type ='text';
     }else{
@@ -53,16 +64,96 @@ function revealBtnHandler(e) {
     }
 }
 
-
-function login(){
-    //Some login stuff
+function formSubmitHandler(e){
+    if(!validateLogin()){
+        e.preventDefault();
+    }
 }
 
+//**************** Functions *************************//
+
+let validations = {
+    required: function(value){
+        return value !== '';
+    },
+    email: function(value){
+        return value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    }
+};
+
+/**
+ * Prototypical validate function
+ * Only checks if inputs are empty
+ * If so adds an error class to form
+ */
+function validateLogin(){
+    for (let el = 0; el<loginElements.$inputs.length; el++){
+        let $input = loginElements.$inputs[el];
+        if(!$input.value) { //Empty Input
+            setFormError(true);
+        }else if($input.id === "email"){
+            if(!validations.email($input.value)){
+                setFormError(true);
+                return false;
+            }
+        }else{
+            setFormError(false);
+            return true;
+        }
+    }
+}
+
+/**
+ * Sets the classes for Error-Look of the form
+ * @param hasError
+ */
+function setFormError(hasError){
+    if (hasError){
+        toggleClass(true, loginElements.$form, 'form--error');
+    }else{
+        toggleClass(false, loginElements.$form, 'form--error');
+    }
+}
+
+/**
+ * Toggles the class to mark an occupied input
+ * @param isOccupied
+ * @param $input
+ */
+function setOccupiedInput(isOccupied, $input){
+    toggleClass(isOccupied, $input, 'form__input--occupied');
+}
+
+/**
+ * Helper to add/remove a class of a element
+ * @param add
+ * @param $el
+ * @param className
+ */
+function toggleClass(add, $el, className){
+    if(add){
+        if(!$el.classList.contains(className)) {
+            $el.classList.add(className);
+        }
+    }else{
+        $el.classList.remove(className);
+    }
+}
 
 function toggleNavi(open){
     if(open){
         //
     }
+}
+
+/**
+ * Helper to check if element has a class
+ * @param element
+ * @param cls
+ * @returns {boolean}
+ */
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
 
